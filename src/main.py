7994818ff30cdd2d390c11f12d795ee87e2f5222
ncorreia47 <dev.ncorr@gmail.com>
2025-05-18 11:56:23 -0300

    update: atualizado metodo de export na main

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from templates.dataframe_to_json import DataFrameToJsonFile
+
+# Exemplo de DataFrame
+df = pd.DataFrame({
+    'nome': ['Ana', 'Bruno'],
+    'idade': [28, 34],
+    'cidade': ['Curitiba', 'São Paulo'],
+    'estado': ['PR', 'SP'],
+    'idade' : [25,32]
+})
+
+DataFrameToJsonFile().export(df=df, file_name='output.json', path_file=None)
